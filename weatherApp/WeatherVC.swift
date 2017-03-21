e9b375f01559f1edd8435097fdff366c3282e65a
Dragoslav Ignjatovic <draza021@gmail.com>
2017-03-21 18:51:28 +0100

    implemented getting data from weather for a single view, day and for forecast

---
+++ weatherApp/WeatherVC.swift
@@ -0,0 +1,86 @@
+//
+//  WeatherVC.swift
+//  weatherApp
+//
+//  Created by Drago on 3/17/17.
+//  Copyright © 2017 Novotek. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class WeatherVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var currentWeather = CurrentWeather()
+    var foreCast: Forecast!
+    var forecasts = [Forecast]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+        currentWeather.downloadWeatherDetails {
+            self.downloadForecastData {
+                self.updateMainUI()
+            }
+            
+        }
+    }
+    
+    func downloadForecastData(completed: @escaping DownloadComplete) {
+        //Downloading weather forecast for table view
+        let forecastURL = URL(string: FORECAST_URL)!
+        Alamofire.request(forecastURL).responseJSON { response in
+            let result = response.result
+            
+            if let dict = result.value as? Dictionary<String, AnyObject> {
+                //get forecast data
+                if let list = dict["list"] as? [Dictionary<String, AnyObject>] {
+                    for obj in list {
+                        let forecast = Forecast(weatherDict: obj)
+                        self.forecasts.append(forecast)
+                        print(obj)
+                    }
+                }
+                
+            }
+            completed()
+        }
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        
+        return cell
+    }
+    
+    func updateMainUI() {
+        dateLabel.text = currentWeather.date
+        currentTempLabel.text = "\(currentWeather.currentTemp)"
+        currentWeatherTypeLabel.text = currentWeather.weatherType
+        locationLabel.text = currentWeather.cityName
+        currentWeatherImage.image = UIImage(named: currentWeather.weatherType)
+    }
+
+}
+
